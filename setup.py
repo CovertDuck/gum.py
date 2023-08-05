c1869978035a794ab41e2f63b42237ea660ad728
CovertDuck <contact@covertduck.io>
2023-08-05 15:20:03 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='gum.py',
+        version='0.1.0-alpha',
+        description='Python bindings for Gum.'
+     )
